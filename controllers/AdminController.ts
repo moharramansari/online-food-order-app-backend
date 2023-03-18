0fb339a4f793618bc055f9eb7f6935c462f3bf5e
Moharram Ansari <moharramansari@gmail.com>
2023-03-18 09:12:59 +0530

    Added new functionalities in the AdminController and recfactror some code

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -3,10 +3,20 @@ import { CreateVandorInput } from "../dto";
 import { Vandor } from "../models";
 import { GenratePassword, GenrateSalt } from "../utility";
 
+
+export const FindVandor = async (id: string | undefined, email?: string) => {
+    if (email) {
+        return await Vandor.findOne({email: email})
+    } else {
+        return await Vandor.findById(id)
+    }
+}
+
+
 export const createVandor = async (req: Request, res: Response, next: NextFunction) => {
     const {name, address, pincode, foodType, email, password, ownerName, phone} = <CreateVandorInput>req.body;
 
-    const existingVandor = await Vandor.findOne({email:email})
+    const existingVandor = await FindVandor('', email);
 
     if (existingVandor !== null) {
         return res.json({"message" : "A vadnor is exist with this email ID"})
@@ -37,9 +47,23 @@ export const createVandor = async (req: Request, res: Response, next: NextFuncti
 }
 
 export const GetVandors = async (req: Request, res: Response, next: NextFunction) => {
-    
+    const vandors = await Vandor.find();
+
+    if (vandors !== null) {
+        return res.json(vandors)
+    }
+
+    return res.json({"message" : "Vandors data not available"})
 }
 
 export const GetVandorByID = async (req: Request, res: Response, next: NextFunction) => {
-    
+    const vandorId = req.params.id;
+
+    const vandor = await FindVandor(vandorId);
+
+    if (vandor !== null) {
+        return res.json(vandor)
+    }
+
+    return res.json({"message" : "Vandor data not available"})
 }
